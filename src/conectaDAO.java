a65609e94308dca48c228c99062beee5be60cf54
Bruno Bretanha <bruno_bretanha@outlook.com>
2024-05-04 23:44:08 -0300

    Modificação Classe CONECTADAO

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,33 +1,58 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import javax.swing.JOptionPane;
 
 
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
     
-    public Connection connectDB(){
-        Connection conn = null;
-        
-        try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11", "root", "Bruno1008961996");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
-        }
-        return conn;
-    }
+   private Connection conexao;
     
+    /**
+     * Obtém a conexão atual.
+     *
+     * @return a conexão atual com o banco de dados.
+     */
+    public Connection getConexao() {
+        return conexao;
+    }
+
+    
+    public void conectar() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexao = DriverManager.getConnection("jdbc:mysql://localhost/uc11", "root", "Bruno1008961996");
+            System.out.println("SUCESSO NA CONEXÃO: ");
+        } catch (ClassNotFoundException e) {
+            System.out.println("FALHA NA CONEXÃO!");
+        } catch (SQLException ex) {
+            System.out.println("Falha ao conectar com o banco. Erro de SQL!");
+        }
+    }
+
+    /**
+     * Encerra a conexão com o banco de dados.
+     *
+     * Este método é usado para encerrar a conexão com o banco de dados após o
+     * término das operações.
+     */
+    public void desconectar() {
+        try {
+            if (conexao != null && !conexao.isClosed()) {
+                conexao.close();
+                System.out.println("DESCONECTADO COM SUCESSO!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro ao desconectar");
+        }
+    }
+
+    public java.sql.PreparedStatement prepareStatement(String sql) throws SQLException {
+    return getConexao().prepareStatement(sql);
 }
+
+}
+
+
